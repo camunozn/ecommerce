cda8ff73a53c14f03f1f99b199d8f34ec9f48480
camunozn <camunozn89@gmail.com>
2023-02-01 18:04:25 -0500

    fixed filter sidebar styles and functionality

--- src/assets/components/SideBar.jsx
+++ src/assets/components/SideBar.jsx
@@ -4,14 +4,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   filterProductsByCategoryThunk,
   filterProductsByPriceThunk,
+  getProductsThunk,
 } from '../../store/slices/products.slice';
 
 const SideBar = () => {
-  const products = useSelector(state => state.products);
   const categories = useSelector(state => state.categories);
   const [openPriceFilter, setOpenPriceFilter] = useState(true);
   const [openCategoryFilter, setOpenCategoryFilter] = useState(true);
-  const [activeCategoryFilter, setActiveCategoryFilter] = useState('');
+  const [activeCategoryFilter, setActiveCategoryFilter] = useState('All');
   const [maxPriceFilter, setMaxPriceFilter] = useState(0);
   const [minPriceFilter, setMinPriceFilter] = useState(0);
 
@@ -57,17 +57,34 @@ const SideBar = () => {
                 onChange={e => setMaxPriceFilter(e.target.value)}
               />
             </div>
-            <button
-              type="button"
-              className="btn btn-primary btn-sm ms-auto"
-              onClick={() =>
-                dispatch(
-                  filterProductsByPriceThunk(parseFloat(minPriceFilter), parseFloat(maxPriceFilter))
-                )
-              }
-            >
-              Filter price
-            </button>
+            <div className="d-flex justify-content-between">
+              <button
+                type="button"
+                className="btn btn-primary btn-sm"
+                onClick={() => {
+                  setActiveCategoryFilter('All');
+                  dispatch(
+                    filterProductsByPriceThunk(
+                      parseFloat(minPriceFilter),
+                      parseFloat(maxPriceFilter)
+                    )
+                  );
+                }}
+              >
+                Filter price
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger btn-sm"
+                onClick={() => {
+                  dispatch(getProductsThunk());
+                  setMinPriceFilter(0);
+                  setMaxPriceFilter(0);
+                }}
+              >
+                Clear filter
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -92,6 +109,20 @@ const SideBar = () => {
         >
           <div className="accordion-body p-0">
             <ul className="list-group w-100 rounded-0">
+              <li
+                className={`list-group-item border-0 ${
+                  activeCategoryFilter === 'All' ? 'active' : ''
+                }`}
+                role="button"
+                onClick={() => {
+                  setMinPriceFilter(0);
+                  setMaxPriceFilter(0);
+                  setActiveCategoryFilter('All');
+                  dispatch(getProductsThunk());
+                }}
+              >
+                All
+              </li>
               {categories.map(category => (
                 <li
                   key={category.id}
@@ -100,6 +131,8 @@ const SideBar = () => {
                   }`}
                   role="button"
                   onClick={() => {
+                    setMinPriceFilter(0);
+                    setMaxPriceFilter(0);
                     setActiveCategoryFilter(category.name);
                     dispatch(filterProductsByCategoryThunk(category.id));
                   }}
