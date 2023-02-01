e2aab8bb688f871fb25403d556d5dbc1dcf06a71
camunozn <camunozn89@gmail.com>
2023-02-01 18:07:10 -0500

    fixed bug in ProductsList component

--- src/assets/components/ProductsList.jsx
+++ src/assets/components/ProductsList.jsx
@@ -9,7 +9,7 @@ const ProductsList = () => {
   return (
     <div className="products-list mb-5">
       <div className={`row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4`}>
-        {products.map(product => (
+        {products?.map(product => (
           <div key={product.id} className="col">
             <div
               role="button"
