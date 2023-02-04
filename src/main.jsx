fa2d29b65d3937873e1d04af1a0d561dafd73bad
camunozn <camunozn89@gmail.com>
2023-02-03 20:34:08 -0500

    modified css theme with bootswatch

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import store from "./store";
-import { Provider } from "react-redux";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import store from './store';
+import { Provider } from 'react-redux';
+import 'bootswatch/dist/flatly/bootstrap.min.css';
+// import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
